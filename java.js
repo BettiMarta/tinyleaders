b6dc413ddc0bd183a48204b26d5e93d2e6ee407d
BettiMarta <161043275+BettiMarta@users.noreply.github.com>
2024-11-04 14:01:10 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+// Notice
+window.alert("This website or its third-party tools use cookies, which are necessary for its functioning and required to achieve the purposes illustrated in the cookie policy.\n\nYou accept the use of cookies by closing this notice, by scrolling this page, by clicking a link or button or by continuing to browseotherwise.");
+
+// Pin Navigation and cover Button Up
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("nav");
+var buttup = document.getElementById("buttonup");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        buttup.classList.remove("visible");
+    } else {
+        header.classList.remove("sticky");
+        buttup.classList.add("visible");
+    }
+}
+
+// Open & close <p> in the "60-30-10 rule" section
+
+function openP(type1, type2){
+    param1 = document.getElementById(type1);
+    param2 = document.getElementById(type2);
+    if(param1.style.display == "none") {
+        param1.style.display = "block";
+        param2.classList.add("rule-open-img");
+    } else {
+        param1.style.display = "none";
+        param2.classList.remove("rule-open-img");
+    }
+}
+
+
+// Open optional window for download
+
+$(document).ready(function(){
+    $("#but-qr").click(function(){
+        $("#qr-window-id").animate({
+            height: 'toggle'
+        });
+    });
+}); 
